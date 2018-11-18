7be14ce478f56828067a27676b83d23a7ca402bb
Yulia <nayanzina@gmail.com>
2018-11-18 14:35:52 +0200

    hw1

--- js/js_hw-01-1/hw-1-1.js
+++ js/js_hw-01-1/hw-1-1.js
@@ -3,19 +3,24 @@
 const adminLogin = "admin";
 const adminPassword = "m4ngo1zh4ackz0r";
 
+const wellcome = "Добро пожаловать!";
+const cancel = "Отменено пользователем!";
+const wrongPassword = "Доступ запрещен, неверный пароль!";
+const wrongLogin = "Доступ запрещен, неверный логин!";
+
 const userLogin = prompt("Введите логин!");
 
 if (userLogin === null) {
-  alert("Отменено пользователем!");
+  alert(cancel);
 } else if (userLogin === adminLogin) {
   const userPassword = prompt("Введите пароль!");
   if (userPassword === null) {
     alert("Отменено пользователем!");
   } else if (userPassword === adminPassword) {
-    alert("Добро пожаловать");
+    alert(wellcome);
   } else {
-    alert("Доступ запрещен, неверный пароль!");
+    alert(wrongPassword);
   }
 } else {
-  alert("Доступ запрещен, неверный логин!");
+  alert(wrongLogin);
 }
